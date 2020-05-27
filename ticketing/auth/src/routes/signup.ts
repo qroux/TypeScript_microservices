f65cfb24cfbd65d16c1755543dc2dd7fbfa0f04e
quentin roux <quentin.roux@hotmail.fr>
2020-05-27 18:10:22 +0200

    routes + validations working

---
+++ ticketing/auth/src/routes/signup.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20 }),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
+
+export { router as signupRouter };
