5d28c6072c136aaf3250627116a267b35b875b4e
quentin roux <quentin.roux@hotmail.fr>
2020-05-29 18:30:27 +0200

    hashing process working

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // Describe required properties in new User (TS)
 interface UserAttrs {
@@ -24,6 +25,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
