713eff1ca5d120539d5a009f7dcca4a90fc2b08e
quentin roux <quentin.roux@hotmail.fr>
2020-06-02 20:32:54 +0200

    error middleware refactored + signin and up working + JWT implementation

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hello');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('You must supply a valid email'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send('Signed in');
+  }
+);
 
 export { router as signinRouter };
