8e35af4dda8ba73982aa9a51af4e557877f4cc2f
quentin roux <quentin.roux@hotmail.fr>
2020-06-04 20:10:53 +0200

    react/next app working

---
+++ ticketing/client/pages/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+  return response.data;
+};
+
+export default LandingPage;
