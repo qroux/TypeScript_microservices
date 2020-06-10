1a17177108fad226c6134143b0f93440bac8110e
quentin roux <quentin.roux@hotmail.fr>
2020-06-10 16:39:18 +0200

    orders service initial setup

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { currentUser, errorHandler, NotFoundError } from '@qroux-corp/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(updateTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
