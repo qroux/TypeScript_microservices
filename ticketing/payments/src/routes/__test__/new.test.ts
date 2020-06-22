58c29c6fb4c221251ecf29c60b11c6da0d468ad5
quentin roux <quentin.roux@hotmail.fr>
2020-06-22 14:45:23 +0200

    payment model + stripe API test working

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -2,7 +2,11 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { Order } from '../../models/order';
+import { Payment } from '../../models/payment';
 import { OrderStatus } from '@qroux-corp/common';
+import { stripe } from '../../stripe';
+
+// jest.mock('../../stripe');
 
 it('returns 404 if order is not found', async () => {
   await request(app)
@@ -60,4 +64,44 @@ it('returns 400 if order already cancelled ', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    version: 0,
+    userId,
+    price,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual('eur');
+  expect(stripeCharge!.amount).toEqual(price * 100);
+
+  const payment = await Payment.findOne({
+    stripeId: stripeCharge!.id,
+    orderId: order.id,
+  });
+
+  expect(payment).not.toBeNull();
+  expect(payment?.orderId).toEqual(order.id);
 });
--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   BadRequestError,
 } from '@qroux-corp/common';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
 import { stripe } from '../stripe';
 
 const router = express.Router();
@@ -33,13 +34,19 @@ router.post(
       throw new BadRequestError('Order expired, cannot pay');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'eur',
       amount: order.price * 100,
       source: token,
     });
 
-    res.send({ success: true });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
+
+    res.status(201).send({ success: true });
   }
 );
 
